5b7f7328ad639afae8ab58b7377236e51d879bae
Matias <youremail@example.com>
2025-11-21 11:42:11 +0100

    j'ai fait les fichier oracle et j'ai commencer a faire le c++

---
+++ main.cpp
@@ -0,0 +1,119 @@
+/**
+ *  @date : 22 octobre 2024
+ *  @author : Alain Casali
+ *  @Brief : une prmière réponse à la SAE 1.02 de 24/25
+**/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * @brief litUneString
+ * @return la chaine lue sauf si :
+ * (1) on une boulette sur l'entrée
+ * (2) on trouve un commentaire sur l'entrée
+ * le commentaire est matérialisé par la chaine "//"
+ */
+
+string litUneString (){
+    string uneChaine;
+    while (true){
+        getline (cin, uneChaine);
+        if ((!cin) || (uneChaine.substr(0,2) != "//")) break;
+    }
+    return uneChaine;
+}
+
+int litUnEntier (){
+    string uneChaine;
+    while (true){
+        getline (cin, uneChaine);
+        if ((!cin) || (uneChaine.substr(0,2) != "//")) break;
+    }
+    return stoi(uneChaine);
+}
+
+struct participant {
+    string nom;
+    string prenom;
+    int glacePref;
+};
+
+bool compare2part (const participant & p1, const participant & p2){
+    return p1.prenom < p2.prenom;
+}
+
+void affichVectString (const vector<string> & v){
+    for (const string & val : v)
+        cout << val << '\t';
+    cout << endl;
+}
+
+void affichVectParticipants (const vector<participant> & vPart){
+    for (const participant & part : vPart){
+        cout << part.nom << endl;
+        cout << part.prenom << endl;
+        cout << part.glacePref << endl;
+    }
+}
+
+int main()
+{
+    //cout << "Hello World!" << endl;
+    //vector<participant> vParticipant (19);
+    /* on va detecter les glaces preférées des votants
+    * tout en virant les commentaires
+    */
+
+    /* on vote sur 4 glaces
+    vector <string> vGlacePref;
+    for (unsigned i (0); i < 4; ++i)
+        vGlacePref.push_back(litUneString());
+
+   debug
+    affichVectString (vGlacePref);
+
+    //On lit les datas du clavier, et on les stocke
+ vector<participant> vParticipant;
+
+    for (unsigned i (0); i < 10; ++i){
+        string nom (litUneString());
+        string prenom  (litUneString());
+        int numGlace (litUnEntier());
+        //cout << nom << endl << prenom << endl << numGlace << endl;
+        vParticipant.push_back(participant{nom, prenom, numGlace});
+              //  vJoueur[numEquipe-1].push_back(joueur {nom, prenom, numEquipe});
+    }
+
+    affichVectParticipants(vParticipant);
+    cout << string (15, '-') << endl;
+    sort (vParticipant.begin(), vParticipant.end(), compare2part);
+    affichVectParticipants(vParticipant);
+
+
+        cout << "c'est la glace " << vGlacePref[0] << " qui a gagne" << endl;
+    return 0; */
+
+    vector<string> candidats;
+    while (true) {
+        string nom = litUneString();
+        if (nom.empty()) break;  // Quand une ligne vide est rencontrée, on a terminé la liste des candidats
+        candidats.push_back(nom);
+    }
+    // Lire le nombre d'électeurs
+    int nbelecteurs = litUnEntier();
+
+    // Lire les classements des électeurs
+    vector<int> classements(nbelecteurs, vector<int>(candidats.size()));
+    for (int i = 0; i < nbelecteurs; i=i+1) {
+        for (int j = 0; j < candidats.size(); j=j+1) {
+            >> classements[i][j];
+        }
+    }
+    int score;
+    for (i=0;i<candidats;i=i+1)
+
+
+}
