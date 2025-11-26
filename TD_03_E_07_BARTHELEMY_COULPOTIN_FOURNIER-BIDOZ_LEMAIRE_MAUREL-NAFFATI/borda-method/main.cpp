d5e858497821b5757ef691fd96b7c8ba462ccfa8
Gaël <gael.barthelemy@etu.univ-amu.fr>
2025-11-26 09:07:47 +0100

    Update main.cpp

--- TD_03_E_07_BARTHELEMY_COULPOTIN_FOURNIER-BIDOZ_LEMAIRE_MAUREL-NAFFATI/borda-method/main.cpp
+++ TD_03_E_07_BARTHELEMY_COULPOTIN_FOURNIER-BIDOZ_LEMAIRE_MAUREL-NAFFATI/borda-method/main.cpp
@@ -28,19 +28,19 @@ int main()
     unsigned nbelecteur = 6;
     vector <string> candidats(nbcandidats);
     vector <string> electeurs(nbelecteur);
-    for(unsigned int i=0;i<=candidats.size()-1; i=i+1)
+    for(unsigned i=0;i<=candidats.size()-1; i=i+1)
     {
         candidats[i]=litUneString();
     }
-    for(unsigned int i=0;i<=electeurs.size()-1; i=i+1)
+    for(unsigned i=0;i<=electeurs.size()-1; i=i+1)
     {
         electeurs[i]=litUneString();
     }
 
     vector<vector<string>> choix(electeurs.size(), vector<string>(candidats.size()));
-    for (unsigned int i=0;i<=choix.size()-1;i=i+1)
+    for (unsigned i=0;i<=choix.size()-1;i=i+1)
     {
-        for (unsigned int j=0;j<=candidats.size()-1;j=j+1)
+        for (unsigned j=0;j<=candidats.size()-1;j=j+1)
         {
             choix[i][j] = litUneString();
         }
@@ -48,16 +48,16 @@ int main()
     }
     unsigned points;
     vector<unsigned> score(nbcandidats);
-    for(unsigned int i=0;i<=candidats.size();i=i+1)
+    for(unsigned i=0;i<=candidats.size();i=i+1)
     {
         score[i]=0;
     }
-    for(unsigned int i=0;i<=choix.size()-1;i=i+1)
+    for(unsigned i=0;i<=choix.size()-1;i=i+1)
     {
 
-        for(unsigned int j=0;j<=candidats.size()-1;j=j+1)
+        for(unsigned j=0;j<=candidats.size()-1;j=j+1)
         {
-            for(unsigned int v=0;v<=candidats.size()-1;v=v+1)
+            for(unsigned v=0;v<=candidats.size()-1;v=v+1)
             {
                 if(choix[i][j]==candidats[v])
                 {
@@ -68,10 +68,10 @@ int main()
 
         }
     }
-    unsigned int maxscore;
+    unsigned maxscore;
     maxscore= 0;
     string gagnant;
-    for (unsigned int i=0;i<=candidats.size()-1;i=i+1)
+    for (unsigned i=0;i<=candidats.size()-1;i=i+1)
     {
         if(score[i]>maxscore)
         {
